14a5344c67faa929c0e3a57373fc5c0260eb76c1
iiuex <iiu@adorsys.com.ua>
2019-06-19 16:14:42 +0300

    Solution

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -16,6 +16,27 @@
  */
 function detectPangram(phrase) {
   // write code here
+  if (!phrase) {
+    return false;
+  }
+
+  const upperPhrase = phrase.toUpperCase();
+  const arr = new Array(26);
+
+  for (let i = 0; i < upperPhrase.length; i++) {
+    if (upperPhrase[i] >= 'A' && upperPhrase[i] <= 'Z'
+      && arr[upperPhrase.charCodeAt(i) - 65] === undefined) {
+      arr[upperPhrase.charCodeAt(i) - 65] = 1;
+    }
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (arr[i] === undefined) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = detectPangram;
