7618f6a82ee6d622f8f6020225a9eaa3992eed11
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-14 11:19:57 +0300

    fix

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -16,11 +16,10 @@
  */
 function detectPangram(phrase) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetLetters = alphabet.split('');
   const phraseLetters = phrase.toLowerCase().split('');
 
-  for (let i = 0; i < alphabetLetters.length; i += 1) {
-    if (phraseLetters.indexOf(alphabetLetters[i]) === -1) {
+  for (let i = 0; i < alphabet.length; i += 1) {
+    if (phraseLetters.indexOf(alphabet[i]) === -1) {
       return false;
     }
   }
