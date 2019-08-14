24e66bb9673c2666ebcded12d663ee90cac71538
Kate Kasiutich <31770149+KateKasiutich@users.noreply.github.com>
2019-08-14 14:20:12 +0300

    Update detect-pangram.js

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -16,9 +16,9 @@
  */
 function detectPangram(phrase) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const strToLowerCase = phrase.toLowerCase();
+  const lowerCaseString = phrase.toLowerCase();
   for (let i = 0; i < alphabet.length; i++) {
-    if (strToLowerCase.indexOf(alphabet[i]) === -1) {
+    if (lowerCaseString.indexOf(alphabet[i]) === -1) {
       return false;
     }
   }
