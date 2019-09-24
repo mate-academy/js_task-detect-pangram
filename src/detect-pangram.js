9e9b04b113042cd1cb231feb0e455235ea613ff8
VadimK <djvadya16@gmail.com>
2019-09-24 19:38:15 +0300

    done

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -15,7 +15,18 @@
  * @return {boolean}
  */
 function detectPangram(phrase) {
-  // write code here
+  const alphabetArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  for (let i = 0; i < alphabetArr.length; i++) {
+    const letterFind = new RegExp(alphabetArr[i], 'gi');
+    if (!phrase.match(letterFind)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = detectPangram;
