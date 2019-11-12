586fdc98dc44847fd9ad526f9900d76fd4c3415e
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-12 15:31:47 +0200

    solution

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -15,7 +15,23 @@
  * @return {boolean}
  */
 function detectPangram(phrase) {
-  // write code here
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const phraseArr = phrase.toLowerCase().split('');
+
+  let counter = 0;
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (phraseArr.includes(alphabet[i])) {
+      counter++;
+    } else {
+      return false;
+    }
+  }
+  if (counter === 26) {
+    return true;
+  }
 }
 
 module.exports = detectPangram;
