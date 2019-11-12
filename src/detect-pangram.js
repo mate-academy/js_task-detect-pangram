5b4abaa57ec331487b322f572ab14caf56cc6abe
Alexandra Boyko <Alexboyko04@gmail.com>
2019-11-12 14:05:04 +0200

    Solution

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -4,8 +4,8 @@
  * Implement detectPangram function:
  *
  * A pangram is a sentence that contains every single letter of the alphabet
- * at least once. For example, the sentence "The quick brown fox jumps over
- * the lazy dog" is a pangram, because it uses the letters A-Z at least once
+ * at least once. For example, the sentence 'The quick brown fox jumps over
+ * the lazy dog' is a pangram, because it uses the letters A-Z at least once
  * (case is irrelevant).
  * Given a string, detect whether or not it is a pangram. Return True if it
  * is, False if not. Ignore numbers and punctuation.
@@ -15,7 +15,18 @@
  * @return {boolean}
  */
 function detectPangram(phrase) {
-  // write code here
+  const lowPhrase = phrase.toLowerCase();
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z'];
+
+  for (let i = 0; i < lowPhrase.length; i++) {
+    while (alphabet.includes(lowPhrase[i])) {
+      alphabet.splice(alphabet[i], 1);
+    }
+  }
+
+  return alphabet.length === 0;
 }
 
 module.exports = detectPangram;
