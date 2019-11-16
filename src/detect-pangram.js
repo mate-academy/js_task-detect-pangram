356b3b8c0280b43bc9a3cc63609bdd12d549b389
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-16 23:11:15 +0200

    Aidinov Alexii v2

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -17,11 +17,11 @@
 function detectPangram(phrase) {
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const newPhrase = phrase.toLowerCase().split('');
-  let matches = '';
+  const normalizedPhrase = phrase.toLowerCase();
+  let matches = 0;
 
   for (let i = 0; i < alphabet.length; i++) {
-    if (newPhrase.includes(alphabet[i])) {
+    if (normalizedPhrase.includes(alphabet[i])) {
       matches++;
     }
   }
