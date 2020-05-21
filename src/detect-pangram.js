c8df12ce2a24a0709c6ff4353f37fd9340480fcc
Liliia <chuiko1112@gmail.com>
2020-05-21 18:55:28 +0300

    Solution

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -15,7 +15,16 @@
  * @return {boolean}
  */
 function detectPangram(phrase) {
-  // write code here
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  let count = 0;
+
+  for (let i = 0; i < alpha.length; i++) {
+    if (phrase.toLowerCase().includes(alpha[i])) {
+      count++;
+    }
+  }
+
+  return count === 26;
 }
 
 module.exports = detectPangram;
