23a76d3ea3b8163cd73180c7002846e28a3fdf9c
denkondratiev <denys.kondratiev.job@gmail.com>
2020-05-21 17:09:05 +0300

    den

--- src/detect-pangram.js
+++ src/detect-pangram.js
@@ -15,7 +15,15 @@
  * @return {boolean}
  */
 function detectPangram(phrase) {
-  // write code here
+  const str = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < str.length; i++) {
+    if (phrase.toLowerCase().indexOf(str[i]) === -1) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = detectPangram;
